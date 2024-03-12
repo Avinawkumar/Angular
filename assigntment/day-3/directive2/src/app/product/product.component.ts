6d559a6d2deebf6ef34090c7e748e5c1c5715ace
Avinaw Kumar <0811kumaravinaw@gmail.com>
2024-03-12 10:41:05 +0530

    assigntment-5

---
+++ assigntment/day-3/directive2/src/app/product/product.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+
+
+export class ProductComponent {
+
+  products: Product[] = [
+    { name: 'Product A', price: 60, available: true },
+    { name: 'Product B', price: 40, available: false },
+    { name: 'Laptop', price: 1200, available: true },
+    { name: 'Smartphone', price: 800, available: true },
+    { name: 'Headphones', price: 100, available: false },
+    { name: 'Camera', price: 600, available: true },
+    { name: 'Tablet', price: 300, available: false },
+    { name: 'Fitness Tracker', price: 50, available: true },
+    { name: 'Bluetooth Speaker', price: 80, available: true },
+    { name: 'Gaming Console', price: 400, available: false },
+    { name: 'Smartwatch', price: 150, available: true },
+    { name: 'External Hard Drive', price: 120, available: true }
+    // Add more products...
+  ];
+
+  priceThreshold: number = 50; // Initialize with default threshold
+
+  
+
+
+}
